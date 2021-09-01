314d524b3f32fb277e1aecfceaaf077124178ec9
john <https://github.com/John78468/dotenv_test.git>
2021-09-01 11:58:30 +0200

    finish new registration style

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
 
-  resources :baskets
   resources :articles
   # devise_for :users 
   devise_for :users
-  resource :basket, only: [:show, :update, :destroy]
+  resources :baskets, only: [:show, :update, :destroy]
   get "/", to: "static_page#home"
   root to: "home#index"
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
