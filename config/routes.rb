2da78d5b687805b34d2447c195454b89cde4b8b4
JulienSisi <julien.sisavath@gmail.com>
2021-09-01 16:43:36 +0200

    back login admins

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
 
+  devise_for :admins, controllers: { sessions: 'admins/sessions' }
   resources :baskets
   resources :articles
-  # devise_for :users 
-  devise_for :users
+#  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
   get "/", to: "static_page#home"
   root to: "home#index"
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
